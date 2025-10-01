88fc7b5a42d2c8f70a5e48a791c67a6393a8cbd7
artemlen <90240896+artemlen@users.noreply.github.com>
2025-10-01 16:46:13 +0000

    Изменил C_ost = 10000000

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ import pandas as pd
 
 k, T, C, L = symbols('k T C L')
 # 1 Способ
-C_ost = 30000 # молодец Артём
+C_ost = 30000  # молодец Артём
 Am_lst = []
-C_ost_lst = [] # Что это такое? это создание пустого списка с именем C_ost_lst, C_ost_lst — это будущий столбец "Остаток стоимости" -5
-for i in range(8): # молодец Артём 2
-  Am = (C - L) / T
-  C_ost -= Am.subs({C: 30000, T: 8, L: 0}) # корректно
-  Am_lst.append(round(Am.subs({C: 30000, T: 8, L: 0}), 2)) # корректно
-  C_ost_lst.append(round(C_ost, 2))
+C_ost_lst = [
+]  # Что это такое? это создание пустого списка с именем C_ost_lst, C_ost_lst — это будущий столбец "Остаток стоимости" -5
+for i in range(8):  # молодец Артём 2
+    Am = (C - L) / T
+    C_ost -= Am.subs({C: 30000, T: 8, L: 0})  # корректно
+    Am_lst.append(round(Am.subs({C: 30000, T: 8, L: 0}), 2))  # корректно
+    C_ost_lst.append(round(C_ost, 2))
 print('Am_lst', Am_lst)
-print('C_ost_lst', C_ost_lst) 
+print('C_ost_lst', C_ost_lst)
 
 #2 способ
 Aj = 0
-C_ost = 30000 # Что это такое? Инициализируем первоначальную стоимость объекта-5 
+C_ost = 10000000  # Что это такое? Инициализируем первоначальную стоимость объекта-5
 Am_lst_2 = []
 C_ost_lst_2 = []
 
 for i in range(8):
-  Am = k * 1 / T * (C - Aj)
-  C_ost -= Am.subs({C: 30000, T: 8, k: 2})
-  Am_lst_2.append(round(Am.subs({C: 30000, T: 8, k: 2}), 2))
-  Aj += Am
-  C_ost_lst_2.append(round(C_ost, 2))
+    Am = k * 1 / T * (C - Aj)
+    C_ost -= Am.subs({C: 30000, T: 8, k: 2})
+    Am_lst_2.append(round(Am.subs({C: 30000, T: 8, k: 2}), 2))
+    Aj += Am
+    C_ost_lst_2.append(round(C_ost, 2))
 
 print('Am_lst_2:', Am_lst_2)
 print('C_ost_lst_2:', C_ost_lst_2)
@@ -35,8 +36,9 @@ import pandas as pd
 
 Y = range(1, 9)
 
-table1 = list(zip(Y, C_ost_lst, Am_lst)) #Зачем эта строка? объединяем три отдельных списка в одну структуру, готовую для создания таблицы, zip соединяет первый элемент из первого списка с первым элементом из второго и третьего списков, второй элемент с вторым и т.д. - получаем список кортежей -5
-
+table1 = list(
+    zip(Y, C_ost_lst, Am_lst)
+)  #Зачем эта строка? объединяем три отдельных списка в одну структуру, готовую для создания таблицы, zip соединяет первый элемент из первого списка с первым элементом из второго и третьего списков, второй элемент с вторым и т.д. - получаем список кортежей -5
 
 table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
 
@@ -58,7 +60,16 @@ labels = list(range(1, 9))
 explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
 
 fig, ax = plt.subplots()
-ax.pie(vals, labels=labels, autopct='%1.1f%%', shadow=True, explode=explode, wedgeprops={'linewidth': 1, 'edgecolor': 'k'}, rotatelabels=True)
+ax.pie(vals,
+       labels=labels,
+       autopct='%1.1f%%',
+       shadow=True,
+       explode=explode,
+       wedgeprops={
+           'linewidth': 1,
+           'edgecolor': 'k'
+       },
+       rotatelabels=True)
 ax.axis('equal')
 plt.show()
 
@@ -80,24 +91,9 @@ ax.pie(vals,
 ax.axis('equal')
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Вариант 2
 from sympy import *
 import pandas as pd
-
-
 
 k, T, C, L = symbols('k T C L')
 # 1 Способ
